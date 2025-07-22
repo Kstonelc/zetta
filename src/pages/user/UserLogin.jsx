70b7827199d89f2b050ba02658dd973cbe6064ea
liuchang <liuchang@yrobot.com>
2025-07-22 18:43:35 +0800

    更新用户登录界面

--- src/pages/user/UserLogin.jsx
+++ src/pages/user/UserLogin.jsx
@@ -10,29 +10,84 @@ import {
   Center,
   Title,
   TextInput,
+  Blockquote,
+  useMantineTheme,
 } from "@mantine/core";
-import { Mail } from "lucide-react";
+import { CircleAlert, CircleUser, Mail, ShieldUser } from "lucide-react";
 import zettaLogo from "@/assets/zetta-logo.svg";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNotify } from "@/utils/notify";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "@mantine/form";
+import appHelper from "@/AppHelper.js";
 
 const UserLogin = () => {
   const { notify } = useNotify();
   const nav = useNavigate();
+  const theme = useMantineTheme();
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      email: "",
+      name: "",
+      password: "",
+    },
 
-  const [userEmail, setUserEmail] = useState("");
+    validate: {
+      name: (value) => (value.trim() ? null : "名不能为空"),
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "邮箱地址无效"),
+      password: (value) =>
+        /^(?=.*[A-Za-z])(?=.*\d).{8,}$/.test(value)
+          ? null
+          : "至少8位, 包含数字和字母",
+    },
+  });
+
+  const [isAdminExist, setIsAdminExist] = useState(false);
+
+  //region 初始化
+  useEffect(() => {
+    initialize();
+    return () => {
+      destroy();
+    };
+  }, []);
+
+  const initialize = async () => {
+    const response = await appHelper.apiPost("/tenant/find-admin");
+    if (response.ok) {
+      setIsAdminExist(true);
+    } else {
+      setIsAdminExist(false);
+    }
+  };
+
+  const destroy = async () => {};
+  //endregion
 
   //region 方法
 
-  const onPressRegister = async () => {
-    nav({
-      pathname: "/user/register",
-    });
-  };
   const onPressReset = async () => {
     nav({
       pathname: "/user/reset-password",
+    });
+  };
+
+  const onPressLogin = async (values) => {
+    const response = await appHelper.apiPost("/user/email-login", {
+      userEmail: values.email,
+      userPassword: values.password,
+    });
+    if (!response.ok) {
+      notify({
+        type: "error",
+        message: response.message,
+      });
+      return;
+    }
+    console.log(response.data);
+    nav({
+      pathname: "/",
     });
   };
 
@@ -41,64 +96,98 @@ const UserLogin = () => {
     <Flex h={"100vh"} p={"xl"} direction={"column"}>
       <Image src={zettaLogo} w={142} h={40} />
       <Center flex={1}>
-        <Card p={"md"} miw={450} withBorder>
-          <Flex direction={"column"} gap={"xs"}>
-            <Title order={3}>🤖 欢迎回来!</Title>
-            <Text c="dimmed" size={"sm"} mb={"md"}>
-              登录 Zetta 以继续
-            </Text>
-            {/*<Group w={"100%"}>*/}
-            {/*  <Button*/}
-            {/*    variant={"default"}*/}
-            {/*    flex={1}*/}
-            {/*    leftSection={<Image src={GithubIcon} w={20} h={20} />}*/}
-            {/*    onClick={() => {*/}
-            {/*      notify({ message: "正在拼命开发中....", type: "warning" });*/}
-            {/*    }}*/}
-            {/*  >*/}
-            {/*    Github*/}
-            {/*  </Button>*/}
-            {/*  <Button*/}
-            {/*    variant={"default"}*/}
-            {/*    flex={1}*/}
-            {/*    leftSection={<Image src={GoogleIcon} w={20} h={20} />}*/}
-            {/*  >*/}
-            {/*    Google*/}
-            {/*  </Button>*/}
-            {/*</Group>*/}
-          </Flex>
-          {/*<Divider my="xs" label="或使用邮箱登录" labelPosition="center" />*/}
-          <Stack mb={"md"}>
-            <TextInput
-              label={"邮箱"}
-              required={true}
-              inputSize={"sm"}
-              onChange={(e) => {
-                setUserEmail(e.target.value);
-              }}
-              value={userEmail}
-            />
-            <TextInput label={"密码"} type={"password"} required={true} />
-            <Button leftSection={<Mail size={16} />}>邮箱登录</Button>
-          </Stack>
-          <Stack gap={"xs"} align={"center"}>
-            <Button variant="transparent" p={0} onClick={onPressReset}>
-              <Text td="underline" size={"sm"} c={"dimmed"}>
-                糟糕, 忘记密码？
+        {isAdminExist ? (
+          <Card p={"md"} miw={450} withBorder>
+            <Flex direction={"column"} gap={"xs"}>
+              <Title order={3}>🤖 欢迎回来!</Title>
+              <Text c="dimmed" size={"sm"} mb={"md"}>
+                登录 Zetta 以继续
               </Text>
-            </Button>
-            <Group gap={0}>
-              <Text c={"dimmed"} size={"sm"}>
-                没有账号？
+            </Flex>
+            <form onSubmit={form.onSubmit((values) => onPressLogin(values))}>
+              <Stack mb={"md"}>
+                <TextInput
+                  label={"邮箱"}
+                  withAsterisk
+                  key={form.key("email")}
+                  inputSize={"sm"}
+                  {...form.getInputProps("email")}
+                />
+                <TextInput
+                  withAsterisk
+                  key={form.key("password")}
+                  label={"密码"}
+                  type={"password"}
+                  {...form.getInputProps("password")}
+                />
+                <Button leftSection={<Mail size={16} />} type="submit">
+                  邮箱登录
+                </Button>
+              </Stack>
+              <Stack gap={"xs"} align={"center"}>
+                <Button variant="transparent" p={0} onClick={onPressReset}>
+                  <Text td="underline" size={"sm"} c={"dimmed"}>
+                    糟糕, 忘记密码？
+                  </Text>
+                </Button>
+              </Stack>
+            </form>
+          </Card>
+        ) : (
+          <Card p={"md"} miw={450} withBorder>
+            <Flex direction={"column"} gap={"xs"}>
+              <Group gap={"xs"}>
+                <ShieldUser size={30} color={theme.colors.blue[6]} />
+                <Title order={3}>设置管理员</Title>
+              </Group>
+              <Text c="dimmed" size={"xs"} mb={"md"}>
+                管理员拥有Zetta最大权限,可用于创建知识库, 配置大模型等
               </Text>
-              <Button variant="transparent" p={0} onClick={onPressRegister}>
-                <Text td="underline" size={"sm"}>
-                  创建一个
-                </Text>
-              </Button>
-            </Group>
-          </Stack>
-        </Card>
+              <form onSubmit={form.onSubmit((values) => {})}>
+                <Stack mb={"md"}>
+                  <TextInput
+                    withAsterisk
+                    label={"姓名"}
+                    key={form.key("name")}
+                    inputSize={"sm"}
+                    placeholder={"姓名"}
+                    {...form.getInputProps("name")}
+                  />
+                  <TextInput
+                    withAsterisk
+                    label={"邮箱"}
+                    key={form.key("email")}
+                    placeholder={"请输入邮箱地址"}
+                    {...form.getInputProps("email")}
+                  />
+                  <TextInput
+                    withAsterisk
+                    type={"password"}
+                    key={form.key("password")}
+                    label={"密码"}
+                    placeholder={"至少8位, 包含数字和字母"}
+                    {...form.getInputProps("password")}
+                  />
+                  <Blockquote
+                    color={theme.colors.yellow[5]}
+                    radius="md"
+                    icon={<CircleAlert size={16} />}
+                    iconSize={30}
+                    p={"sm"}
+                    mt="xs"
+                  >
+                    <Text size={"sm"}>
+                      密码要求：至少8位字符，包含大小写字母、数字
+                    </Text>
+                  </Blockquote>
+                  <Button leftSection={<CircleUser size={18} />} type="submit">
+                    创建账户
+                  </Button>
+                </Stack>
+              </form>
+            </Flex>
+          </Card>
+        )}
       </Center>
     </Flex>
   );
