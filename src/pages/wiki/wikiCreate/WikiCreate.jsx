5ddca60c48a07da64a42200e5508f70c96421bac
liuchang <liuchang@yrobot.com>
2025-08-04 18:42:54 +0800

    update

--- src/pages/wiki/wikiCreate/WikiCreate.jsx
+++ src/pages/wiki/wikiCreate/WikiCreate.jsx
@@ -212,7 +212,7 @@ const WikiCreate = () => {
       </Group>
 
       <WikiCreateCancelModal
-        opened={true}
+        opened={false}
         onClose={() => {}}
       ></WikiCreateCancelModal>
     </Stack>
