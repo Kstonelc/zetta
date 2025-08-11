f32114acbc695dbcfa55589d82894c6a2e73ec98
liuchang <liuchang@yrobot.com>
2025-08-11 18:56:46 +0800

    agent界面布局

--- src/pages/agent/Agent.jsx
+++ src/pages/agent/Agent.jsx
@@ -4,41 +4,95 @@ import {
   Text,
   Group,
   Flex,
-  AppShell,
+  Select,
+  Title,
+  ActionIcon,
+  TextInput,
   useMantineTheme,
+  Menu,
+  Card,
   Button,
   Paper,
   Divider,
+  ScrollArea,
 } from "@mantine/core";
 import { ChatBox } from "@/components";
 
 import classes from "./Agent.module.scss";
+import { MessagesSquare, FileUp, Box, Sparkles, ArrowUp } from "lucide-react";
 import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone";
 
 const Agent = () => {
   const theme = useMantineTheme();
 
   return (
-    <Stack p={"lg"} flex={1}>
-      <Dropzone
-        w={400}
-        onDrop={(files) => console.log("accepted files", files)}
-        onReject={(files) => console.log("rejected files", files)}
-        maxSize={5 * 1024 ** 2}
-        accept={IMAGE_MIME_TYPE}
-      >
-        <Group
-          justify="center"
-          gap="xl"
-          mih={220}
-          style={{ pointerEvents: "none" }}
+    <Flex p={"lg"} flex={1} gap={"sm"}>
+      <Card maw={300} miw={200} shadow={"md"} withBorder={true} h={"100%"}>
+        <Button mb={"md"} leftSection={<MessagesSquare size={16} />}>
+          新建对话
+        </Button>
+        <ScrollArea>
+          <Menu width={200}>
+            <Menu.Label>今天</Menu.Label>
+            <Menu.Item>新对话</Menu.Item>
+          </Menu>
+        </ScrollArea>
+      </Card>
+      <Divider orientation={"vertical"} />
+      <Stack h={"100%"} flex={1} align={"center"} justify={"flex-end"}>
+        <Title
+          order={1}
+          style={{
+            position: "absolute",
+            top: "30%",
+          }}
         >
-          <Text size="xl" inline>
-            上传文件
-          </Text>
-        </Group>
-      </Dropzone>
-    </Stack>
+          我是Zetta智能助手，开始聊天吧
+        </Title>
+        <Card shadow={"sm"} w={"70%"} mb={"xl"} withBorder>
+          <TextInput
+            variant={"unstyled"}
+            placeholder={"请输入你的问题"}
+            w={"100%"}
+            size={"md"}
+            mb={"xs"}
+          />
+          <Group justify={"space-between"}>
+            <Group>
+              <ActionIcon variant={"subtle"} bg={theme.colors.gray[1]}>
+                <FileUp size={18} color={theme.colors.gray[7]} />
+              </ActionIcon>
+              <Select
+                maw={120}
+                leftSectionPointerEvents="none"
+                leftSection={
+                  <Box
+                    size={16}
+                    color={theme.colors.gray[7]}
+                    style={{
+                      marginTop: 2,
+                    }}
+                  />
+                }
+                data={["Deepseek", "Qwen"]}
+                size={"xs"}
+              ></Select>
+              <Button
+                variant="gradient"
+                leftSection={<Sparkles size={16} />}
+                size={"xs"}
+                gradient={{ from: "grape", to: "cyan", deg: 90 }}
+              >
+                深度思考
+              </Button>
+            </Group>
+            <ActionIcon size="lg" variant={"light"} bdrs={"lg"}>
+              <ArrowUp size={24} />
+            </ActionIcon>
+          </Group>
+        </Card>
+      </Stack>
+    </Flex>
   );
 };
 
--- src/pages/user/UserLogin.jsx
+++ src/pages/user/UserLogin.jsx
@@ -4,7 +4,6 @@ import {
   Card,
   Button,
   Group,
-  Divider,
   Image,
   Stack,
   Center,
