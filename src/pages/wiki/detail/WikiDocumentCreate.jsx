a6b1b1661bac4a98ee33e63988cd8a482cccc3a9
liuchang <liuchang@yrobot.com>
2025-11-27 09:37:08 +0800

    update

--- src/pages/wiki/detail/WikiDocumentCreate.jsx
+++ src/pages/wiki/detail/WikiDocumentCreate.jsx
@@ -6,19 +6,118 @@ import {
   Title,
   Divider,
   Image,
+  Card,
+  Stepper,
+  ActionIcon,
+  useMantineTheme,
 } from "@mantine/core";
 import { Dropzone } from "@mantine/dropzone";
-import { ArrowLeft, ArrowRight } from "lucide-react";
+import { ArrowLeft, ArrowRight, Trash2 } from "lucide-react";
 import InBox from "/inbox.svg";
 import { useNavigate } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useNotify } from "@/utils/notify.js";
+import appHelper from "@/AppHelper.js";
+import { FileType } from "@/enum.js";
+import classes from "@/pages/wiki/wikiCreate/WikiCreate.module.scss";
 
 const WikiDocumentCreate = () => {
   const nav = useNavigate();
   const { notify } = useNotify();
+  const theme = useMantineTheme();
 
+  const [currentStep, setCurrentStep] = useState(1);
   const [isUploading, setIsUploading] = useState(false);
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+
+  const currentUploadedFilesRef = useRef([]);
+
+  // region 方法
+  const onUploadFile = async (files) => {
+    const fileMap = new Map();
+    for (const file of files) {
+      const response = await appHelper.apiPost("/wiki/upload-file", file);
+      if (!response.ok) {
+        notify({
+          type: "error",
+          message: response.message,
+        });
+      } else {
+        const fileInfo = response.data;
+        const fileName = fileInfo.fileName;
+        fileMap.set(fileName, fileInfo);
+      }
+    }
+    const results = Array.from(fileMap.values());
+    setUploadedFiles((prev) => {
+      const prevMap = new Map();
+      for (const item of prev) {
+        if (!fileMap.has(item.fileName)) {
+          prevMap.set(item.fileName, item);
+        }
+      }
+      currentUploadedFilesRef.current = [...prevMap.values(), ...results];
+      return currentUploadedFilesRef.current;
+    });
+    setIsUploading(false);
+  };
+
+  const onDeleteFile = (fileName) => {
+    setUploadedFiles((prev) => {
+      currentUploadedFilesRef.current = prev.filter(
+        (item) => item.fileName !== fileName,
+      );
+      return prev.filter((item) => item.fileName !== fileName);
+    });
+  };
+
+  // endregion
+
+  // region 组件渲染
+  const renderFileIcon = (fileExt) => {
+    return (
+      <Image src={FileType.icon[FileType.getFileType(fileExt)]} w={20} h={20} />
+    );
+  };
+
+  const renderUploadedFiles = () => {
+    if (appHelper.getLength(uploadedFiles) > 0) {
+      return uploadedFiles.map((file, index) => {
+        const fileType = FileType.getFileType(file.fileExt);
+        let fileSize = file.fileSize;
+        fileSize = appHelper.getFileSize(fileSize);
+        return (
+          <Card miw={300} maw={600} withBorder p={"xs"} mb={"xs"} key={index}>
+            <Group justify={"space-between"}>
+              <Group gap={"sm"}>
+                {renderFileIcon(file.fileExt)}
+                <div>
+                  <Text size={"xs"} fw={"bold"}>
+                    {file.fileName}
+                  </Text>
+                  <Text size={"xs"} c={"dimmed"}>
+                    {FileType.text[fileType]}&nbsp;&nbsp;{fileSize}
+                  </Text>
+                </div>
+              </Group>
+              <ActionIcon
+                variant={"subtle"}
+                size={"sm"}
+                color={theme.colors.gray[6]}
+                onClick={() => {
+                  onDeleteFile(file.fileName);
+                }}
+              >
+                <Trash2 size={16} />
+              </ActionIcon>
+            </Group>
+          </Card>
+        );
+      });
+    }
+  };
+
+  // endregion
 
   return (
     <Stack p={"lg"} flex={1} gap={"xs"}>
@@ -32,6 +131,18 @@ const WikiDocumentCreate = () => {
         >
           返回知识库
         </Button>
+        <Stepper
+          iconSize={24}
+          active={currentStep}
+          allowNextStepsSelect={false}
+          size={"xs"}
+          className={classes.absoluteCenter}
+          miw={500}
+        >
+          <Stepper.Step label="数据上传" />
+          <Stepper.Step label="数据分段" />
+          <Stepper.Step label="数据处理" />
+        </Stepper>
       </Group>
       <Divider mb={"xl"}></Divider>
       <Stack px={"100"}>
@@ -39,7 +150,7 @@ const WikiDocumentCreate = () => {
         <Dropzone
           onDrop={async (files) => {
             setIsUploading(true);
-            // await onUploadFile(files);
+            await onUploadFile(files);
           }}
           onReject={(files) => {
             for (const file of files) {
@@ -63,7 +174,6 @@ const WikiDocumentCreate = () => {
             "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
               [".docx"],
           }}
-          mb={"sm"}
           miw={300}
           maw={600}
         >
@@ -80,8 +190,14 @@ const WikiDocumentCreate = () => {
             </div>
           </Group>
         </Dropzone>
+        {renderUploadedFiles()}
         <Group>
-          <Button leftSection={<ArrowRight size={16} />}>下一步</Button>
+          <Button
+            leftSection={<ArrowRight size={16} />}
+            disabled={appHelper.getLength(uploadedFiles) === 0}
+          >
+            下一步
+          </Button>
         </Group>
       </Stack>
     </Stack>
